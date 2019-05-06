dbbf0b32cb35007a2e7598c57a3f5d84ce5fbb9a
Silvia Sirbu <ssilvias15@yahoo.com>
2019-05-06 20:51:23 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//https://github.com/h5bp/html5-boilerplate/blob/v4.3.0/js/plugins.js
+// Avoid `console` errors in browsers that lack a console.
+
+(function() {
+
+    var method;
+    var noop = function () {};
+
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+
+    ];
+
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+
+}());
+// Place any jQuery/helper plugins in here.
+
+
+
+$( document ).ready(function() {
+
+    
+    $(window).on('resize', function() {
+        
+    });
+
+});
